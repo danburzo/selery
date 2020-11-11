b91390214601763225311e92cf07e6696668acde
Dan Burzo <dan@danburzo.ro>
2020-11-11 21:28:19 +0200

    Add support for microsyntaxes in pseudo-classes and pseudo-elements (#6)
    * Refactor out recursive parsing options, and walker
    * Make provisions for microsyntax support in pseudo-elements
    * Fix a few An+B related tokenization bugs, treat AnPlusB syntax as None for now
    * Update documentation

---
+++ test/testcases/anplusb.case.js
@@ -0,0 +1,65 @@
+/*
+	Tests for the An+B microsyntax.
+ */
+
+export default [
+	{
+		selector: ':nth-child(odd), :nth-child(even)',
+		serialize: true,
+		parse: {
+			type: 'SelectorList',
+			selectors: [
+				{
+					type: 'PseudoClassSelector',
+					identifier: 'nth-child',
+					argument: [{ type: 'ident', value: 'odd' }]
+				},
+				{
+					type: 'PseudoClassSelector',
+					identifier: 'nth-child',
+					argument: [{ type: 'ident', value: 'even' }]
+				}
+			]
+		}
+	},
+	{
+		selector: ':nth-last-of-type(2n+3)',
+		serialize: true,
+		parse: {
+			type: 'SelectorList',
+			selectors: [
+				{
+					type: 'PseudoClassSelector',
+					identifier: 'nth-last-of-type',
+					argument: [
+						{ type: 'dimension', value: '2', unit: 'n' },
+						{ type: 'number', value: '+3' }
+					]
+				}
+			]
+		}
+	},
+	{
+		selector: ':nth-child(2 of :is(wow))',
+		serialize: true,
+		parse: {
+			type: 'SelectorList',
+			selectors: [
+				{
+					type: 'PseudoClassSelector',
+					identifier: 'nth-child',
+					argument: [
+						{ type: 'number', value: '2' },
+						{ type: 'whitespace' },
+						{ type: 'ident', value: 'of' },
+						{ type: 'whitespace' },
+						{ type: 'colon' },
+						{ type: 'function', value: 'is' },
+						{ type: 'ident', value: 'wow' },
+						{ type: ')' }
+					]
+				}
+			]
+		}
+	}
+];
