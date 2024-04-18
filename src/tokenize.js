8ffa370f127af3e1d5acf76bf213c60f042846fc
Dan Burzo <dan@danburzo.ro>
2024-04-18 23:29:04 +0300

    Forgot about some unicode ranges for idents

--- src/tokenize.js
+++ src/tokenize.js
@@ -1,15 +1,9 @@
 // https://drafts.csswg.org/css-syntax/#ident-start-code-point
 const IdentStartCodePoint =
-	/[a-zA-Z_]|[\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
+	/[a-zA-Z_]|[\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u{10000}-\u{10FFFF}]/u;
 const IdentCodePoint =
-	/[-\w]|[\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
+	/[-\w]|[\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u{10000}-\u{10FFFF}]/u;
 const HexDigit = /[0-9a-zA-Z]/;
-
-/*
-
-
-    greater than or equal to U+10000
-*/
 
 export const Tokens = {
 	AtKeyword: 'at-keyword',
